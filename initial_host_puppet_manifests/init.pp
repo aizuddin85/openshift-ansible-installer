b4bea777a4134fd0bb555ab07d23eade90d6a60c
root <root@oc-master-east1-a.c.secret-device-149711.internal>
2017-07-15 06:09:37 +0000

    add puppet manifest to setup ssh

---
+++ initial_host_puppet_manifests/init.pp
@@ -0,0 +1,29 @@
+Exec { path => '/bin:/usr/bin' }
+
+exec { "ssh_keygen-root" :
+	command => "ssh-keygen -t rsa -b 2048 -N '' -f /root/.ssh/id_rsa",
+}
+
+exec { "copy_authorized_keys" :
+	command => "cat  /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys",
+}
+
+exec { "fix permisson for authorized_keys" :
+	command => "chmod 400 /root/.ssh/id_rsa.pub",
+}
+
+service { 'sshd':
+	ensure => 'running',
+	enable => true,
+}
+
+file { '/etc/ssh/sshd_config':
+	ensure => present,
+}-> file_line {'make sure sshd permit the rootl login via pubkey':
+	replace => true,
+	path => '/etc/ssh/sshd_config',
+	line => 'PermitRootLogin without-password',
+	match => "^PermitRootLogin.*$",
+	notify => Service['sshd'],
+}
+
