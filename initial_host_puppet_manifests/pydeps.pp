25e8aa3fc8bd1ae542ac2b50d4cbf78a6b488918
root <root@openshift.c.openshift-179205.internal>
2017-11-14 08:39:49 +0000

    update

--- initial_host_puppet_manifests/pydeps.pp
+++ initial_host_puppet_manifests/pydeps.pp
@@ -1,4 +1,4 @@
-$pydeps = [ 'java-1.8.0-openjdk-headless', 'pyOpenSSL', 'python-passlib', 'python2-cryptography', 'bind-utils' ]
+$pydeps = [ 'java-1.8.0-openjdk-headless', 'pyOpenSSL', 'python-passlib', 'python2-cryptography', 'bind-utils', 'python-rhsm-certificates' ]
 
 package { $pydeps:
 	ensure => 'installed',
